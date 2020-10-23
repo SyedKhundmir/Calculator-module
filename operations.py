0beef9c06cd02a379e661bad461a84d76f57581c
SyedKhundmir <69012420+SyedKhundmir@users.noreply.github.com>
2020-10-23 23:40:42 +0530

    Create operations.py

---
+++ operations.py
@@ -0,0 +1,19 @@
+def add(sum,val):
+    res = sum + val
+    return res
+
+def sub(diff,val):
+    res = diff - val
+    return res
+
+def mul(data,val):
+    res =  data * val
+    return res
+
+def div(data,val):
+    res = data / val
+    return res
+
+def mod(data,val):
+    res = data % val
+    return res
